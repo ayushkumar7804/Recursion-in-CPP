47fb196658e2eaf0a5b783be65b269e7e8136e58
KassArc <ayushkumaryadav2029@gmail.com>
2025-09-17 11:27:31 +0530

    Create FactorialUsingRecursion.cpp

---
+++ FactorialUsingRecursion.cpp
@@ -0,0 +1,27 @@
+// Ayush Yadav
+// 24070123028
+
+
+#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+    if (n <= 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    cout << "Factorial of " << num << " is " << factorial(num) << endl;
+    return 0;
+}
+
+/*Output
+Enter a number: 5
+Factorial of 5 is 120
+  */
